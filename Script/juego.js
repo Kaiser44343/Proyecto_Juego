3d2f35687a1f0235cd04a2374ec05fc67c07d23b
kaiser <antonioruben183@gmail.com>
2026-04-07 09:15:02 +0200

    primer commit

---
+++ Script/juego.js
@@ -0,0 +1,15 @@
+const cont = document.getElementById("continue")
+
+cont.addEventListener("click", ()=>{
+    document.body.style.backgroundColor = "cyan";
+})
+const nuevo = document.getElementById("NewG")
+
+nuevo.addEventListener("click", ()=>{
+    document.body.style.backgroundColor = "red";
+})
+const opt = document.getElementById("Opts")
+
+opt.addEventListener("click", ()=>{
+    document.body.style.backgroundColor = "black";
+})
